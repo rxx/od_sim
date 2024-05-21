4783d639d427999bd523eeaf2be3b792f4390623
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-05-21 17:55:57 +0000

    Setup go

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
-package main
+  package main
 
-import (
-  "fmt"
-)
+  import (
+      "fmt"
 
-func main() {
-	fmt.Println("Hello, world!")
-}
+      "github.com/xuri/excelize/v2"
+  )
+
+  func main() {
+      f, err := excelize.OpenFile("data/sim.xlsm")
+      if err != nil {
+          fmt.Println(err)
+          return
+      }
+      defer func() {
+          // Close the spreadsheet.
+          if err := f.Close(); err != nil {
+              fmt.Println(err)
+          }
+      }()
+      // Get value from cell by given worksheet name and cell reference.
+      cell, err := f.GetCellValue("Overview", "B14")
+      if err != nil {
+          fmt.Println(err)
+          return
+      }
+      fmt.Println(cell)
+    
+  }
